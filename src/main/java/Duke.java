f5f60b48ce7a6ad890fc70dca116a91546469388
Wilfred Bradley Tan <wilfredbtan@gmail.com>
2019-08-16 15:39:07 +0800

    do Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,26 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("____________________________________________________________");
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________");
+
+        Scanner sc = new Scanner(System.in);
+        while(true) {
+            String word = sc.next();
+            if (word.equals("bye")) {
+                System.out.println("____________________________________________________________");
+                System.out.println("  Bye. Hope to see you again soon!");
+                System.out.println("____________________________________________________________");
+                sc.close();
+                break;
+            } else {
+                System.out.println("____________________________________________________________");
+                System.out.println("  " + word);
+                System.out.println("____________________________________________________________");
+            }
+        }
     }
 }
